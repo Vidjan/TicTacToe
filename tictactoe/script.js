07e021ed4d0395d049750f860902126473d57062
TimTrojner <35002774+TimTrojner@users.noreply.github.com>
2021-01-13 10:14:12 +0100

    test commit

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -87,7 +87,7 @@ $(() => {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 // Is the spot available?
-                if (board[i][j] == '') {
+                if (board[i][j] === '') {
                     board[i][j] = ai;
                     let score = script(board, 0, false);
                     board[i][j] = '';
